4e562771fd163a94cac8baf22e59cc1c6ee53fe8
Vanyo <vanyolai@gmail.com>
2026-09-11 13:08:51 +0200

    feat: register supplier invoice point-of-tax hook

--- core/modules/modNavInvoice.class.php
+++ core/modules/modNavInvoice.class.php
@@ -25,7 +25,7 @@ class modNavInvoice extends DolibarrModules
         $this->descriptionlong = 'ModuleNavInvoiceDesc';
         $this->editor_name = 'vanyolai';
         $this->editor_url = 'https://github.com/vanyolai/dolibarr-nav-online-invoice';
-        $this->version = '0.7.1';
+        $this->version = '0.7.2';
         $this->const_name = 'MAIN_MODULE_'.strtoupper($this->name);
         $this->picto = 'file-invoice';
 
@@ -38,7 +38,7 @@ class modNavInvoice extends DolibarrModules
             'models' => 0,
             'css' => array(),
             'js' => array(),
-            'hooks' => array(),
+            'hooks' => array('invoicesuppliercard'),
             'moduleforexternal' => 0,
         );
 
