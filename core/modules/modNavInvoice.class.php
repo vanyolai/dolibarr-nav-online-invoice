bae40fa114296569a54671a0c5d1e29495a3a234
Vanyo <vanyolai@gmail.com>
2026-09-11 22:47:33 +0200

    CHORE bump module version to 0.8.0

--- core/modules/modNavInvoice.class.php
+++ core/modules/modNavInvoice.class.php
@@ -25,7 +25,7 @@ class modNavInvoice extends DolibarrModules
         $this->descriptionlong = 'ModuleNavInvoiceDesc';
         $this->editor_name = 'vanyolai';
         $this->editor_url = 'https://github.com/vanyolai/dolibarr-nav-online-invoice';
-        $this->version = '0.7.2';
+        $this->version = '0.8.0';
         $this->const_name = 'MAIN_MODULE_'.strtoupper($this->name);
         $this->picto = 'file-invoice';
 
