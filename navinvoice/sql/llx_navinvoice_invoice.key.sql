f6a0062377fda73810d4c82cf5e57fb3ee4e1d33
Vanyo <vanyolai@gmail.com>
2026-09-11 08:36:06 +0200

    refactor: remove nested module indexes

--- navinvoice/sql/llx_navinvoice_invoice.key.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE llx_navinvoice_invoice ADD UNIQUE INDEX uk_navinvoice_invoice (entity, invoice_number, batch_index);
-ALTER TABLE llx_navinvoice_invoice ADD INDEX idx_navinvoice_issue_date (entity, invoice_issue_date);
-ALTER TABLE llx_navinvoice_invoice ADD INDEX idx_navinvoice_customer_tax (entity, customer_tax_number);
-ALTER TABLE llx_navinvoice_invoice ADD INDEX idx_navinvoice_fk_facture (fk_facture);
