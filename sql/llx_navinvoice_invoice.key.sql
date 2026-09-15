62424d310f1854a3c2d75323c5ea70fa142200d6
Vanyo <vanyolai@gmail.com>
2026-09-16 01:04:58 +0200

    Make supplier part of NAV mirror unique key

--- sql/llx_navinvoice_invoice.key.sql
+++ sql/llx_navinvoice_invoice.key.sql
@@ -1,4 +1,4 @@
-ALTER TABLE llx_navinvoice_invoice ADD UNIQUE INDEX uk_navinvoice_invoice (entity, invoice_direction, invoice_number, batch_index);
+ALTER TABLE llx_navinvoice_invoice ADD UNIQUE INDEX uk_navinvoice_invoice (entity, invoice_direction, supplier_tax_number, invoice_number, batch_index);
 ALTER TABLE llx_navinvoice_invoice ADD INDEX idx_navinvoice_issue_date (entity, invoice_issue_date);
 ALTER TABLE llx_navinvoice_invoice ADD INDEX idx_navinvoice_customer_tax (entity, customer_tax_number);
 ALTER TABLE llx_navinvoice_invoice ADD INDEX idx_navinvoice_supplier_tax (entity, supplier_tax_number);
