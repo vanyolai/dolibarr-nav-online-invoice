2e525c30d80e44d3c5a5a250158e4045dcaaf016
Vanyo <vanyolai@gmail.com>
2026-09-22 12:42:47 +0200

    refactor: pin live NAV reads to production profile

--- class/navtaxpayer.class.php
+++ class/navtaxpayer.class.php
@@ -17,7 +17,7 @@ class NavTaxpayerService
 
     public function __construct(?NavInvoiceApi $api = null)
     {
-        $this->api = $api ?: new NavInvoiceApi();
+        $this->api = $api ?: NavInvoiceApi::production();
     }
 
     /**
